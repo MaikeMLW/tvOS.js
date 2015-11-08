ab39f54309574ea951df4ee37a62cda4dd9e4794
Wesley de Groot <wes@vista.aero>
2015-11-08 20:02:54 +0100

    tvOS.js

---
+++ example_loading.js
@@ -0,0 +1,27 @@
+if (App === 'undefined') var App = {} // Eslint error.
+if (tvOS === 'undefined') var tvOS = {} // Eslint error.
+if (evaluateScripts === 'undefined') var evaluateScripts = {} // Eslint error.
+
+evaluateScripts(['https://wesdegroot.github.io/tvOS.js/tvOS.js'], function (success) {
+  if (success) {
+    //
+    // Show loading indicator with default text ('Loading')...
+    tvOS.showLoadingIndicator()
+    //
+    // Timer!
+    setTimeout(function () {
+      // Remove Loading indicator...
+      tvOS.removeLoadingIndicator()
+      // Display Loading screen with text Reloading
+      tvOS.showLoadingIndicator('Please Wait')
+      // Reload timer ;)
+      setTimeout(function () {
+        //
+        // Finally. Reload
+        tvOS.location('https://wesdegroot.github.io/tvOS.js/index.js')
+      }, 3000)
+    }, 1000)
+  } else {
+    console.log('Missing it all!')
+  }
+})
