588b06f3482a1457c8f3a5ceca9263a41a85256a
Wesley de Groot <wes@vista.aero>
2015-11-08 18:29:58 +0100

    tvOS.js

--- example_compilation.js
+++ example_compilation.js
@@ -2,7 +2,7 @@ if (App === 'undefined') var App = {} // Eslint error.
 if (tvOS === 'undefined') var tvOS = {} // Eslint error.
 if (evaluateScripts === 'undefined') var evaluateScripts = {} // Eslint error.
 
-evaluateScripts(['http://localhost:9001/tvOS.js'], function (success) {
+evaluateScripts(['https://wesdegroot.github.io/tvOS.js/tvOS.js'], function (success) {
   if (success) {
     var myList = []
     var myButtons = []
